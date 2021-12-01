757705877b806b9643d1424a6ea442e6ef8ed3a9
damoklov <mishanya@protonmail.com>
2021-12-02 01:40:41 +0200

    Updated with axios

--- src/components/update.js
+++ src/components/update.js
@@ -1,66 +1,68 @@
 import React, { useState, useEffect } from 'react';
 import { Button, Checkbox, Form } from 'semantic-ui-react'
-import {API} from "aws-amplify";
 import { useLocation } from 'react-router-dom'
+import axios from "axios";
 
 export default function Update() {
-    const location = useLocation()
+    const location = useLocation();
     const [id, setID] = useState(null);
-    const [name, setName] = useState('');
-    const [developer, setDeveloper] = useState('');
-    const [online, setOnline] = useState(false);
-    const [started, setStarted] = useState('');
-    const [registered, setRegistered] = useState('');
-    const [browser, setBrowser] = useState('');
+    const [timestamp, setTimestamp] = useState('');
+    const [sensor_id, setSensor_id] = useState('');
+    const [sensor_type, setSensor_type] = useState('');
+    const [cpu_load, setCpu_load] = useState(0);
+    const [gpu_load, setGpu_load] = useState(0);
+    const [fps, setFps] = useState(0);
+    const [api_key, setApi_key] = useState("");
 
     useEffect(() => {
         setID(location.data);
     }, [location.data]);
 
     const updateAPIData = () => {
-        API.put('gamestoreapi', `/game/${id}`, {
-            body: {
-                name: name,
-                developer: developer,
-                online: online,
-                started: started,
-                registered: registered,
-                browser: browser
-            }
-        })
+        const body = {
+            timestamp: timestamp,
+            sensor_id: sensor_id,
+            sensor_type: sensor_type,
+            cpu_load: cpu_load,
+            gpu_load: gpu_load,
+            fps: fps,
+            API_KEY: api_key
+        };
+        axios.put(`http://backend:5000/iot/${id}`, {body})
     }
 
-    return (
+return (
         <div>
             <Form className="create-form">
                 <Form.Field>
-                    <label>ID</label>
-                    <input placeholder='Name' value={id} onChange={(e) => setName(e.target.value)}/>
+                    <label>Timestamp</label>
+                    <input placeholder='Timestamp' onChange={(e) => setTimestamp(e.target.value)}/>
                 </Form.Field>
                 <Form.Field>
-                    <label>Game</label>
-                    <input placeholder='Name' value={name} onChange={(e) => setName(e.target.value)}/>
+                    <label>Sensor ID</label>
+                    <input placeholder='ID' onChange={(e) => setSensor_id(e.target.value)}/>
                 </Form.Field>
                 <Form.Field>
-                    <label>Developer</label>
-                    <input placeholder='Developer' value={developer} onChange={(e) => setDeveloper(e.target.value)}/>
+                    <label>Sensor Type</label>
+                    <input placeholder='Type' onChange={(e) => setSensor_type(e.target.value)}/>
                 </Form.Field>
                 <Form.Field>
-                    <label>Started Date</label>
-                    <input placeholder='Started' value={started} onChange={(e) => setStarted(e.target.value)}/>
+                    <label>CPU Load</label>
+                    <input placeholder='Load' onChange={(e) => setCpu_load(e.target.value)}/>
                 </Form.Field>
                 <Form.Field>
-                    <label>Registered Date</label>
-                    <input placeholder='Registered' value={registered} onChange={(e) => setRegistered(e.target.value)}/>
+                    <label>GPU Load</label>
+                    <input placeholder='Load' onChange={(e) => setGpu_load(e.target.value)}/>
                 </Form.Field>
                 <Form.Field>
-                    <label>Browser</label>
-                    <input placeholder='Browser' value={browser} onChange={(e) => setBrowser(e.target.value)}/>
+                    <label>FPS</label>
+                    <input placeholder='FPS' onChange={(e) => setFps(e.target.value)}/>
                 </Form.Field>
                 <Form.Field>
-                    <Checkbox label='I agree to the Terms and Conditions' onChange={(e) => setOnline(!online)}/>
+                    <label>API Key</label>
+                    <input placeholder='Key' onChange={(e) => setApi_key(e.target.value)}/>
                 </Form.Field>
-                <Button type='submit' onClick={updateAPIData}>Update</Button>
+                <Button onClick={updateAPIData} type='submit'>Update</Button>
             </Form>
         </div>
     )
